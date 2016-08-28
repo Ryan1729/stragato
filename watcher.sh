176f5093638201162e605c5d57a58a34e65bbd8d
Ryan1729 <Ryan1729@gmail.com>
2016-08-28 13:48:24 -0600

    Deal with lack of namespacing in BOOT COMMANDS
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- watcher.sh
+++ watcher.sh
@@ -3,4 +3,10 @@
 # reload:
 # https://www.npmjs.com/package/reload
 
-./build/parallel_commands "reload" "./build/run_filewatcher.sh"
+# I renamed it to reload0 to deal with this issue:
+# https://github.com/jprichardson/reload/issues/13
+# which is about a command run at boot time in Ubuntu having the same name.
+# I never actually tried to see if it affected 16.04 but didn't seem like it
+# was worth the risk.
+
+./build/parallel_commands "reload0" "./build/run_filewatcher.sh"
