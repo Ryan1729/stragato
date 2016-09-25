a63736a9a9cd4b0dcf5d65c2c51a68c4ebb41797
Ryan1729 <Ryan1729@gmail.com>
2016-09-25 14:42:33 -0600

    adjust layout of dev controls
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- DevControls.elm
+++ DevControls.elm
@@ -9,6 +9,7 @@ import Material.Scheme
 import Material.Options as Options exposing (css)
 import Material.Button as Button
 import Material.Icon as Icon
+import Material.List as Lists
 import Material.Slider as Slider
 import Material.Tabs as Tabs
 import Material.Table as Table
@@ -59,94 +60,101 @@ make model =
 
 editTab : Model -> Html Msg
 editTab model =
-    grid []
-        [ cell [ size All 4 ]
-            [ Button.render Msg.Mdl
-                [ 1 ]
-                model.mdl
-                [ Button.onClick Msg.GenerateBoard
-                , css "margin" "0 24px"
-                ]
-                [ Icon.i "cached"
-                , Options.span [ css "width" "4px" ] []
-                , text "Generate Board"
+    div []
+        [ grid []
+            [ cell [ size All 4 ]
+                [ Button.render Msg.Mdl
+                    [ 1 ]
+                    model.mdl
+                    [ Button.onClick Msg.GenerateBoard
+                    , css "margin" "0 24px"
+                    ]
+                    [ Icon.i "cached"
+                    , Options.span [ css "width" "4px" ] []
+                    , text "Generate Board"
+                    ]
                 ]
             ]
-        , toggleSwitchCell [ 8 ]
-            model.mdl
-            Msg.ToggleSpaceOutlines
-            "Show outlines of empty spaces"
-            model.showSpaceOutlines
-        , toggleSwitchCell [ 9 ]
-            model.mdl
-            Msg.ToggleSelfMoves
-            "Allow \"moving\" to same space"
-            model.allowSelfMoves
-        , toggleSwitchCell [ 10 ]
-            model.mdl
-            Msg.ToggleAllowMovingAllPieces
-            "Allow moving all pieces"
-            model.allowMovingAllPieces
-        , toggleSwitchCell [ 11 ]
-            model.mdl
-            Msg.ToggleIgnoreGameResult
-            "Allow moving pieces after end of game"
-            model.ignoreGameResult
-          -- , cell [ size All 4 ] []
-        , cell [ size All 4 ]
-            <| makeStepper [ 12 ]
+        , grid []
+            [ toggleSwitchCell [ 8 ]
                 model.mdl
-                "win condition"
-                DecrementWinCon
-                IncrementWinCon
-                (Model.getWinConString model)
-        , cell [ size All 4 ]
-            <| makeStepper [ 13 ]
+                Msg.ToggleSpaceOutlines
+                "Show outlines of empty spaces"
+                model.showSpaceOutlines
+            , toggleSwitchCell [ 9 ]
                 model.mdl
-                "loss condition"
-                DecrementLossCon
-                IncrementLossCon
-                (Model.getLossConString model)
-        , cell [ size All 4 ]
-            <| makeStepper [ 5 ]
+                Msg.ToggleSelfMoves
+                "Allow \"moving\" to same space"
+                model.allowSelfMoves
+            , toggleSwitchCell [ 10 ]
                 model.mdl
-                "gridWidth"
-                DecrementGridWidth
-                IncrementGridWidth
-                (toString model.gridWidth)
-        , cell [ size All 4 ]
-            <| makeStepper [ 6 ]
+                Msg.ToggleAllowMovingAllPieces
+                "Allow moving all pieces"
+                model.allowMovingAllPieces
+            , toggleSwitchCell [ 11 ]
                 model.mdl
-                "gridHeight"
-                DecrementGridHeight
-                IncrementGridHeight
-                (toString model.gridHeight)
-        , cell [ size All 4 ]
-            <| makeStepper [ 7 ]
-                model.mdl
-                "scale"
-                DecrementViewScale
-                IncrementViewScale
-                (toString model.viewScale)
-        , cell [ size All 6 ]
-            [ deckControl [ 2 ]
-                model.mdl
-                Spaces.spaceTypePossibilities
-                model.spaceDeck
-                Msg.SpaceDeckDecrement
-                Msg.SpaceDeckIncrement
-                (positionedSvgMakerToHtmlMaker
-                    <| Playfield.space model.showSpaceOutlines [ stroke "grey" ]
-                )
+                Msg.ToggleIgnoreGameResult
+                "Allow moving pieces after end of game"
+                model.ignoreGameResult
             ]
-        , cell [ size All 6 ]
-            [ deckControl [ 3 ]
-                model.mdl
-                Pieces.pieceTypePossibilities
-                model.pieceDeck
-                Msg.PieceDeckDecrement
-                Msg.PieceDeckIncrement
-                (positionedSvgMakerToHtmlMaker <| Playfield.piece [ stroke "grey" ])
+        , grid []
+            [ cell [ size All 4 ]
+                <| makeStepper [ 12 ]
+                    model.mdl
+                    "win condition"
+                    DecrementWinCon
+                    IncrementWinCon
+                    (Model.getWinConString model)
+            , cell [ size All 4 ]
+                <| makeStepper [ 13 ]
+                    model.mdl
+                    "loss condition"
+                    DecrementLossCon
+                    IncrementLossCon
+                    (Model.getLossConString model)
+            , cell [ size All 4 ]
+                <| makeStepper [ 5 ]
+                    model.mdl
+                    "gridWidth"
+                    DecrementGridWidth
+                    IncrementGridWidth
+                    (toString model.gridWidth)
+            , cell [ size All 4 ]
+                <| makeStepper [ 6 ]
+                    model.mdl
+                    "gridHeight"
+                    DecrementGridHeight
+                    IncrementGridHeight
+                    (toString model.gridHeight)
+            , cell [ size All 4 ]
+                <| makeStepper [ 7 ]
+                    model.mdl
+                    "scale"
+                    DecrementViewScale
+                    IncrementViewScale
+                    (toString model.viewScale)
+            ]
+        , grid []
+            [ cell [ size All 6 ]
+                [ deckControl [ 2 ]
+                    model.mdl
+                    Spaces.spaceTypePossibilities
+                    model.spaceDeck
+                    Msg.SpaceDeckDecrement
+                    Msg.SpaceDeckIncrement
+                    (positionedSvgMakerToHtmlMaker
+                        <| Playfield.space model.showSpaceOutlines [ stroke "grey" ]
+                    )
+                ]
+            , cell [ size All 6 ]
+                [ deckControl [ 3 ]
+                    model.mdl
+                    Pieces.pieceTypePossibilities
+                    model.pieceDeck
+                    Msg.PieceDeckDecrement
+                    Msg.PieceDeckIncrement
+                    (positionedSvgMakerToHtmlMaker <| Playfield.piece [ stroke "grey" ])
+                ]
             ]
         ]
 
@@ -169,20 +177,26 @@ makeStepper : List Int -> Mdl -> String -> Msg -> Msg -> String -> List (Html Ms
 makeStepper index mdl label decrementMsg incrementMsg value =
     [ text label
     , div [ style [ ( "border", "1px solid" ) ] ]
-        [ Button.render Msg.Mdl
-            (index ++ [ 0 ])
-            mdl
-            [ Button.onClick decrementMsg
-            ]
-            [ Icon.i "remove"
-            ]
-        , text value
-        , Button.render Msg.Mdl
-            (index ++ [ 1 ])
-            mdl
-            [ Button.onClick incrementMsg
-            ]
-            [ Icon.i "add"
+        [ Html.ul [ style [ ( "list-style", "none" ), ( "padding", "0" ), ( "margin", "0" ) ] ]
+            [ Html.li []
+                [ Button.render Msg.Mdl
+                    (index ++ [ 1 ])
+                    mdl
+                    [ Button.onClick incrementMsg
+                    ]
+                    [ Icon.i "add"
+                    ]
+                ]
+            , Html.li [ style [ ( "margin-left", "2em" ) ] ] [ text value ]
+            , Html.li []
+                [ Button.render Msg.Mdl
+                    (index ++ [ 0 ])
+                    mdl
+                    [ Button.onClick decrementMsg
+                    ]
+                    [ Icon.i "remove"
+                    ]
+                ]
             ]
         ]
     ]
